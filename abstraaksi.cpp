9799cf502f9710e009589713729c11e50d62a826
Akmall10 <akmalprasety53@gmail.com>
2025-06-02 18:17:52 +0700

    kelas joko

--- abstraaksi.cpp
+++ abstraaksi.cpp
@@ -6,5 +6,17 @@ class AbstraksiKlas
 private:
     string x, y;
 
-public
+public:
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
+    // menampilkan nilai
+    void display()
+    {
+        cout << "x =" << x << endl;
+    }
 }
--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class seseorang
-{
-public:
-    // virtual void pesan() = 0;
-    virtual void pesan()
-    {
-        cout << "Pesan dari seseorang" << endl;
 
-    }
-};
 
 class joko : public seseorang
 {
@@ -35,10 +26,11 @@ int main()
     lia b;
 
     obyek = &a;
-    obyek ->pesan();
+     obyek ->pesan();
     obyek = &a;
     obyek ->pesan();
     // a.seseorang::pesan();
     // b.seseorang::pesan();
 
+    return 0;
 }
