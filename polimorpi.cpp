0b0c252aca0d2328fa26a94b3b2e55206d14b491
Akmall10 <akmalprasety53@gmail.com>
2025-06-02 18:19:51 +0700

    kelas seseorang

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class seseorang
-{
-public:
-    // virtual void pesan() = 0;
-    virtual void pesan()
-    {
-        cout << "Pesan dari seseorang" << endl;
 
-    }
-};
 
 class joko : public seseorang
 {
@@ -28,4 +19,18 @@ class lia : public seseorang
     }
 };
 
+int main()
+{
+    seseorang *obyek;
+    joko a;
+    lia b;
 
+    obyek = &a;
+     obyek ->pesan();
+    obyek = &a;
+    obyek ->pesan();
+    // a.seseorang::pesan();
+    // b.seseorang::pesan();
+
+    return 0;
+}
